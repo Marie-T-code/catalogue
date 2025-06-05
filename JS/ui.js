8ee54e4926f3bae57ecf564d8d2ddf0e262eb25b
Marie-T-code <thomas.marie8@gmail.com>
2025-06-05 16:57:55 +0200

    jeudi 05 06 layout done

--- JS/ui.js
+++ JS/ui.js
@@ -4,3 +4,23 @@ const nav = document.querySelector('.contacts_header');
 burger.addEventListener('click', () =>{
     nav.classList.toggle('open');
 });
+
+const toggleBtn = document.querySelectorAll('.toggle-theme'); 
+
+toggleBtn.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    const isActive = toggle.classList.contains('active');
+
+    toggleBtn.forEach(t => {
+      t.classList.remove('active', 'reverse');
+    });
+
+    if (isActive) {
+      toggleBtn.forEach(t => t.classList.add('reverse'));
+      document.body.classList.remove('darkMode');
+    } else {
+      toggleBtn.forEach(t => t.classList.add('active'));
+      document.body.classList.add('darkMode');
+    }
+  });
+});
